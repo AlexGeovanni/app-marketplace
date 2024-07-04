5067fef9f8697952e36bf0e223afb5d366da4a17
Alex G <A180065@unach.mx>
2024-07-04 11:45:10 -0700

    component: checkbox en parte de filtros

--- src/components/category/Box/box-check.tsx
+++ src/components/category/Box/box-check.tsx
@@ -1,19 +1,28 @@
 
+interface Props extends React.HTMLAttributes<HTMLInputElement>{
+  text:string
+  name:string
+  selectedCheckbox:number
+  handleCheckboxChange:(index:number)=>void;
+  numberOfCheck:number
+}
 
 
-export default function BoxCheck(){
-    return (
-        <div className="form-check">
-                <input
-                  className="form-check-input"
-                  type="checkbox"
-                  value=""
-                  id="flexCheckChecked"
-                  checked
-                />
-                <label className="form-check-label" htmlFor="flexCheckChecked">
-                  Checked checkbox
-                </label>
-              </div>
-    )
+export default function BoxCheck({selectedCheckbox,handleCheckboxChange,numberOfCheck, id,name,text}:Props) {
+  return (
+    <div className="form-check">
+      <input
+        className="form-check-input"
+        type="checkbox"
+        name={name}
+        value=""
+        id={id}
+        checked={selectedCheckbox === numberOfCheck}
+          onChange={() => handleCheckboxChange(numberOfCheck)}
+      />
+      <label className="form-check-label" htmlFor={id}>
+       {text}
+      </label>
+    </div>
+  );
 }
--- src/components/category/accordion-modal.tsx
+++ src/components/category/accordion-modal.tsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import BoxCheck from "./Box/box-check";
 
 export default function AccordionModal() {
+  const [selectedCheckbox, setSelectedCheckbox] = useState(1);
+
+  const handleCheckboxChange = (index: number) => {
+    setSelectedCheckbox(index);
+  };
+
   return (
     <>
       <div className="accordion " id="accordionPanelsStayOpenExample">
@@ -22,15 +29,51 @@ export default function AccordionModal() {
             className="accordion-collapse collapse"
           >
             <div className="accordion-body">
-              <BoxCheck />
-              <BoxCheck />
-              <BoxCheck />
-              <BoxCheck />
-              <BoxCheck />
+              <BoxCheck
+                selectedCheckbox={selectedCheckbox}
+                handleCheckboxChange={handleCheckboxChange}
+                numberOfCheck={1}
+                
+                id="flexCheckChecked1"
+                name="flexCheckBusiness"
+                text="Negocio1"
+              />
+              <BoxCheck
+                selectedCheckbox={selectedCheckbox}
+                handleCheckboxChange={handleCheckboxChange}
+                numberOfCheck={2}
+                id="flexCheckChecked2"
+                name="flexCheckBusiness"
+                text="Negocio2"
+              />
+              <BoxCheck
+                selectedCheckbox={selectedCheckbox}
+                handleCheckboxChange={handleCheckboxChange}
+                numberOfCheck={3}
+                id="flexCheckChecked3"
+                name="flexCheckBusiness"
+                text="Negocio3"
+              />
+              <BoxCheck
+                selectedCheckbox={selectedCheckbox}
+                handleCheckboxChange={handleCheckboxChange}
+                numberOfCheck={4}
+                id="flexCheckChecked4"
+                name="flexCheckBusiness"
+                text="Negocio4"
+              />
+              <BoxCheck
+                selectedCheckbox={selectedCheckbox}
+                handleCheckboxChange={handleCheckboxChange}
+                numberOfCheck={5}
+                id="flexCheckChecked5"
+                name="flexCheckBusiness"
+                text="Negocio5"
+              />
             </div>
           </div>
         </div>
-        <div className="accordion-item border-0 border-bottom rounded-0">
+        {/* <div className="accordion-item border-0 border-bottom rounded-0">
           <h2 className="accordion-header">
             <button
               className="accordion-button shadow-none collapsed rounded-0"
@@ -48,11 +91,31 @@ export default function AccordionModal() {
             className="accordion-collapse collapse"
           >
             <div className="accordion-body">
-            <BoxCheck />
-              <BoxCheck />
-              <BoxCheck />
-              <BoxCheck />
-              <BoxCheck />
+              <BoxCheck
+                id="flexCheckChecked6"
+                name="flexCheckBrad"
+                text="marca1"
+              />
+              <BoxCheck
+                id="flexCheckChecked7"
+                name="flexCheckBrad"
+                text="marca2"
+              />
+              <BoxCheck
+                id="flexCheckChecked8"
+                name="flexCheckBrad"
+                text="marca3"
+              />
+              <BoxCheck
+                id="flexCheckChecked9"
+                name="flexCheckBrad"
+                text="marca4"
+              />
+              <BoxCheck
+                id="flexCheckChecked10"
+                name="flexCheckBrad"
+                text="marca5"
+              />
             </div>
           </div>
         </div>
@@ -74,14 +137,35 @@ export default function AccordionModal() {
             className="accordion-collapse collapse"
           >
             <div className="accordion-body">
-            <BoxCheck />
-              <BoxCheck />
-              <BoxCheck />
-              <BoxCheck />
-              <BoxCheck />
+              <BoxCheck
+                id="flexCheckChecked11"
+                name="flexCheckCategory"
+                defaultChecked
+                text="Categoria1"
+              />
+              <BoxCheck
+                id="flexCheckChecked12"
+                name="flexCheckCategory"
+                text="Categoria2"
+              />
+              <BoxCheck
+                id="flexCheckChecked13"
+                name="flexCheckCategory"
+                text="Categoria3"
+              />
+              <BoxCheck
+                id="flexCheckChecked14"
+                name="flexCheckCategory"
+                text="Categoria4"
+              />
+              <BoxCheck
+                id="flexCheckChecked15"
+                name="flexCheckCategory"
+                text="Categoria5"
+              />
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     </>
   );
