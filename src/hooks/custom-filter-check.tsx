cba30061d2fedbb90c5dff47cc33352a91577cee
Alex Geovanni <106128149+AlexGeovanni@users.noreply.github.com>
2024-07-08 12:51:15 -0700

    Actualizar custom-filter-check.tsx
    Cambian el topado de checkbox en custom 1.2

--- src/hooks/custom-filter-check.tsx
+++ src/hooks/custom-filter-check.tsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 
 export default function CustomFilterCheckbox() {
-  const [selectedCheckbox, setSelectedCheckbox] = useState<string|number|null>("all");
+  const [selectedCheckbox, setSelectedCheckbox] = useState<number|null>(1);
 
  
-  const handleCheckboxChange = (index: string) => {
+  const handleCheckboxChange = (index: number) => {
     setSelectedCheckbox(index);
   };
 
